b802c310a2a5649798c5127d3c91a3f7f57ccbca
Ludovic FAVIER <lfavier@live.fr>
2021-02-14 16:09:36 +0100

    change update 5

--- routes/marketsCoins.js
+++ routes/marketsCoins.js
@@ -45,34 +45,34 @@ const marketRoutes = (app, fs) => {
   });
 
   // UPDATE
-  // app.put('/marketsCoins/:id', (req, res) => {
-  //   if(req.body.ID === undefined ||
-  //     req.body.NAME === undefined ||
-  //     req.body.PRICE_USD === undefined ||
-  //     req.body.PRICE_EUR === undefined ||
-  //     req.body.IMAGEURL === undefined) {
-  //       return res.status(400).end();
-  //   }
-  //   readFile(data => {
-  //     const found = data.find(market => market.ID == req.params.id);
-  //     if(!found) {
-  //       return res.status(404).end();
-  //     }
-  //     //TODO test data[found]
-  //     // const userId = req.params["id"];
-  //     // data[userId] = req.body;
-  //     data.found.ID = req.body.ID,
-  //     data.found.NAME = req.body.NAME,
-  //     data.found.PRICE_USD = req.body.PRICE_USD,
-  //     data.found.PRICE_EUR = req.body.PRICE_EUR,
-  //     data.found.IMAGEURL = req.body.IMAGEURL,
-  //     data.found.COMMENTS = req.body.COMMENTS,
+  app.put('/marketsCoins/:id', (req, res) => {
+    if(req.body.ID === undefined ||
+      req.body.NAME === undefined ||
+      req.body.PRICE_USD === undefined ||
+      req.body.PRICE_EUR === undefined ||
+      req.body.IMAGEURL === undefined) {
+        return res.status(400).end();
+    }
+    readFile(data => {
+      const found = data.find(market => market.ID == req.params.id);
+      if(!found) {
+        return res.status(404).end();
+      }
+      //TODO test data[found]
+      // const userId = req.params["id"];
+      // data[userId] = req.body;
+      found.ID = req.body.ID,
+      found.NAME = req.body.NAME,
+      found.PRICE_USD = req.body.PRICE_USD,
+      found.PRICE_EUR = req.body.PRICE_EUR,
+      found.IMAGEURL = req.body.IMAGEURL,
+      found.COMMENTS = req.body.COMMENTS,
 
-  //     writeFile(JSON.stringify(data, null, 2), () => {
-  //       res.status(201).send(`market id:${req.params.id} updated`);
-  //     });
-  //   }, true);
-  // });
+      writeFile(JSON.stringify(data, null, 2), () => {
+        res.status(201).send("market updated");
+      });
+    }, true);
+  });
   
   // CREATE
   app.post('/marketsCoins', (req, res) => {
@@ -95,22 +95,6 @@ const marketRoutes = (app, fs) => {
     });
     }, true);
   });
-  // app.post('/users', (req, res) => {
-
-  //   readFile(data => {
-  //       // Note: this isn't ideal for production use. 
-  //       // ideally, use something like a UUID or other GUID for a unique ID value
-  //       const newUserId = Date.now().toString();
-
-  //       // add the new user
-  //       data[newUserId.toString()] = req.body;
-
-  //       writeFile(JSON.stringify(data, null, 2), () => {
-  //           res.status(200).send('new user added');
-  //       });
-  //   },
-  //       true);
-  // });
 
   // DELETE
   app.delete('/marketsCoins/:id', (req, res) => {
@@ -126,20 +110,6 @@ const marketRoutes = (app, fs) => {
       });
     }, true);
   });
-  // app.delete('/marketsCoins/:id', (req, res) => {
-
-  //   readFile(data => {
-
-  //       // add the new user
-  //       const userId = req.params["id"];
-  //       delete data[userId];
-
-  //       writeFile(JSON.stringify(data, null, 2), () => {
-  //           res.status(200).send(`users id:${userId} removed`);
-  //       });
-  //   },
-  //       true);
-  // });
 };
 
 module.exports = marketRoutes;
